f8de73b7e2817bd5f120d86b02b45903ff753dcd
Alex Kordas <aekordas@gmail.com>
2022-03-09 23:03:14 -0500

    Chapter 1 and 2 Notes + Assignments
    Chapter 1 and 2 Notes + Assignments

---
+++ Chapter 2/chapter2assignment.py
@@ -0,0 +1,9 @@
+# The code below almost works
+name = input("Enter your name: ")
+print("Hello",name)
+
+# This first line is provided for you
+hrs = input("Enter Hours: ")
+rate = 2.75
+pay = rate * float(hrs)
+print("Pay: ",pay)
